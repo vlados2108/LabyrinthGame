491f9704ce002876c381d9e8450eeb573a18c359
vlados2108 <verlov.vladislav@mail.ru>
2023-11-17 00:02:44 +0300

    added semicolon to backgroundcolor property of color div to avoid warning

--- src/app/Components/ColorCell/ColorCell.tsx
+++ src/app/Components/ColorCell/ColorCell.tsx
@@ -9,7 +9,7 @@ interface ColorCellProps{
 export default function ColorCell({color,cellName}:ColorCellProps) {
   return (
     <div className={styles['color-container']}>
-        <div className={styles['color']} style={{backgroundColor:color}}/>
+        <div className={styles['color']} style={{"backgroundColor":color}}/>
         <div className={styles['color-cell-name']}>{cellName}</div>
     </div>
   )
