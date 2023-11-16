84eebfb4685fe77659c921e6e7b54320ceb84296
vlados2108 <verlov.vladislav@mail.ru>
2023-11-17 02:13:45 +0300

    turned back next config and deleted dist

--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sDrr4IUiw83YOFl7slb6VH+Mj6YCguceAqtnXEQCLmk=\"\n}"
--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,
-    output: 'export',
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    skipTrailingSlashRedirect: true,
-
-    // Optional: Change the output directory `out` -> `dist`
-    distDir: 'dist',
-    basePath: '/github-pages',
+    // trailingSlash: true,
+    // output: 'export',
+    //basePath: '/github-pages',
 }
 
 module.exports = nextConfig
