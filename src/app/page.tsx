c7629951906bd2aa83e5959e97c8e9b2a7ca177d
vlados2108 <verlov.vladislav@mail.ru>
2023-11-16 19:19:06 +0300

    added algorithm for finding shortests path, seting start and finish points

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,11 +18,12 @@ const COLS = 100
 
 const PathfindingApp = () => {
     const [grid, setGrid] = useState<Cell[]>([])
-    const [startNode, setStartNode] = useState({ row: 5, col: 5 })
-    const [finishNode, setFinishNode] = useState({ row: 15, col: 15 })
+    const [startNode, setStartNode] = useState(0)
+    const [finishNode, setFinishNode] = useState(9999)
     const [isRunning, setIsRunning] = useState(false)
     const [executionTime, setExecutionTime] = useState(0)
-
+    const [startSeting, setStartSeting] = useState(false)
+    const [finishSeting, setFinishSeting] = useState(false)
     useEffect(() => {
         initializeGrid()
     }, [])
@@ -32,8 +33,8 @@ const PathfindingApp = () => {
             return {
                 row: Math.floor(index / ROWS),
                 col: index % ROWS,
-                isStart: false,
-                isFinish: false,
+                isStart: index === 0 ? true : false,
+                isFinish: index === 9999 ? true : false,
                 isWall: false,
                 distance: Infinity,
                 isVisited: false,
@@ -43,30 +44,143 @@ const PathfindingApp = () => {
         setGrid(grid)
     }
 
-    const visualizePathfinding = () => {
+    const getNeighbors = (node: Cell) => {
+        const { row, col } = node
+        const neighbors = []
+        if (row > 0) neighbors.push(grid[(row - 1) * COLS + col])
+        if (row < ROWS - 1) neighbors.push(grid[(row + 1) * COLS + col])
+        if (col > 0) neighbors.push(grid[row * COLS + col - 1])
+        if (col < COLS - 1) neighbors.push(grid[row * COLS + col + 1])
+        return neighbors.filter((neighbor) => !neighbor.isWall)
+    }
+
+    const visualizePath = (path: Cell[]) => {
+        path.forEach((node) => {
+            node.isVisited = true
+        })
+        setGrid([...grid])
+    }
+
+    const findPath = () => {
         if (!isRunning) {
             const startTime = performance.now()
 
+            const start = grid[startNode]
+            const finish = grid[finishNode]
+            const openSet: Cell[] = [start]
+            const closedSet: Cell[] = []
+
+            start.distance = 0 
+
+            while (openSet.length > 0) {
+                let currentNode = openSet[0]
+                let currentIndex = 0
+
+                openSet.forEach((node, index) => {
+                    if (
+                        node.distance + heuristic(node, finish) <
+                        currentNode.distance + heuristic(currentNode, finish)
+                    ) {
+                        currentNode = node
+                        currentIndex = index
+                    }
+                })
+
+                if (
+                    currentNode.row === finish.row &&
+                    currentNode.col === finish.col
+                ) {
+                    const path = []
+                    let current = currentNode
+                    while (current !== start) {
+                        path.unshift(current)
+                        current = current.previousNode!
+                    }
+                    const endTime = performance.now()
+                    const timeElapsed = endTime - startTime
+                    setExecutionTime(timeElapsed)
+                    visualizePath(path)
+                    return path
+                }
+
+                openSet.splice(currentIndex, 1)
+                closedSet.push(currentNode)
+
+                const neighbors = getNeighbors(currentNode)
+                neighbors.forEach((neighbor) => {
+                    if (!closedSet.includes(neighbor) && !neighbor.isWall) {
+                        const tempDistance = currentNode.distance + 1
+                        if (tempDistance < neighbor.distance) {
+                            neighbor.distance = tempDistance
+                            neighbor.previousNode = currentNode
+                            if (!openSet.includes(neighbor))
+                                openSet.push(neighbor)
+                        }
+                    }
+                })
+            }
+           
             const endTime = performance.now()
             const timeElapsed = endTime - startTime
             setExecutionTime(timeElapsed)
+            return []
         }
+    }
+
+    const heuristic = (nodeA: Cell, nodeB: Cell) => {
+        return Math.abs(nodeA.row - nodeB.row) + Math.abs(nodeA.col - nodeB.col)
     }
 
     const toggleWall = (ind: number) => {
         if (!isRunning) {
             const newGrid = grid.slice()
-            //if (!newGrid[row][col].isStart && !newGrid[row][col].isFinish)
-            newGrid[ind].isWall = !newGrid[ind].isWall
-            console.log(newGrid[ind])
+            if (startSeting) {
+                newGrid[startNode].isStart = false
+                newGrid[ind].isStart = !newGrid[ind].isStart
+                setStartSeting(false)
+                setStartNode(ind)
+            }
+            if (finishSeting) {
+                newGrid[finishNode].isFinish = false
+                newGrid[ind].isFinish = !newGrid[ind].isFinish
+                setFinishSeting(false)
+                setFinishNode(ind)
+            }
+            if (!newGrid[ind].isStart && !newGrid[ind].isFinish)
+                newGrid[ind].isWall = !newGrid[ind].isWall
             setGrid(newGrid)
         }
+    }
+
+    const setStart = () => {
+        setStartSeting(!startSeting)
+        if (finishSeting) setFinishSeting(false)
+    }
+    const setEnd = () => {
+        setFinishSeting(!finishSeting)
+        if (startSeting) setStartSeting(false)
     }
 
     return (
         <div className={styles['main-container']}>
             <h1>Pathfinding Visualizer</h1>
-            <button onClick={visualizePathfinding}>Построить маршрут</button>
+            <div className={styles['main-btns-container']}>
+                <button onClick={findPath}>
+                    Построить маршрут
+                </button>
+                <button
+                    className={`${startSeting && styles.active}`}
+                    onClick={setStart}
+                >
+                    Поставить старт
+                </button>
+                <button
+                    className={`${finishSeting && styles.active}`}
+                    onClick={setEnd}
+                >
+                    Поставить финиш
+                </button>
+            </div>
 
             <div className={styles['grid-container']}>
                 {grid.map((el, index) => {
@@ -77,7 +191,7 @@ const PathfindingApp = () => {
                                 el.isWall && styles.wall
                             } ${el.isStart && styles.start} ${
                                 el.isFinish && styles.finish
-                            }`}
+                            } ${el.isVisited && !el.isFinish && styles.visited}`}
                             onClick={() => toggleWall(index)}
                         ></div>
                     )
